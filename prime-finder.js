409a44447d0194316b122b91adec14d1d4956024
Frank <fr.heggeness@gmail.com>
2019-04-17 09:40:51 -1000

    done

--- prime-finder.js
+++ prime-finder.js
@@ -1,3 +1,25 @@
 module.exports = function(n) {
-  // do work here
+  var primeArray = [2];
+  let number = 3;
+  let div;
+  let i = undefined;
+  let limit = undefined;
+  let isPrime = undefined;
+  while (primeArray.length < n) {
+    div = 3;
+    i = 1;
+    limit = Math.sqrt(number) + 1;
+    isPrime = true;
+    while (div < limit) {
+      if (number % div === 0) {
+        isPrime = false;
+        div = limit;
+      } else div = primeArray[i++] || div + 2;
+    }
+    if (isPrime) {
+      primeArray.push(number);
+    }
+    number += 2;
+  }
+  return primeArray[primeArray.length - 1];
 };
